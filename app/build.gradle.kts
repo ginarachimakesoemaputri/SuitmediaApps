631c97d14bd305fa447cf8f079c51696572b2750
ginarachimakesoemaputri <ginarachima@gmail.com>
2023-12-30 01:04:43 +0800

    create the interface on xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,21 @@ plugins {
 
 android {
     namespace = "com.suitmedia.suitmediaapp"
-    compileSdk = 34
+    compileSdk = 31
 
     defaultConfig {
         applicationId = "com.suitmedia.suitmediaapp"
         minSdk = 19
-        targetSdk = 34
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -44,4 +49,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    //retrofit-api
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
